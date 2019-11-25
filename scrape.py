4b2d71f3f1d4c43ec88c9d5393ca1b748f93d8c0
Yashvardhan Jain <yashvardhan18@yahoo.com>
2019-11-25 07:50:54 +0530

    Modified Scraper

--- scrape.py
+++ scrape.py
@@ -4,9 +4,10 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait as wait
 from selenium.webdriver.support.expected_conditions import presence_of_element_located
 import time
+from bs4 import BeautifulSoup
 
 global driver
-driver = wd.Chrome('chromedriver')
+driver = wd.Firefox()
 
 def get_source(departure,destination,dd,mm,yyyy):
     url = 'https://flight.yatra.com/air-search-ui/dom2/trigger?type=O&viewName=normal&flexi=0&noOfSegments=1&origin=' + str(departure)+ '&originCountry=IN&destination=' + str(destination) + '&destinationCountry=IN&flight_depart_date='+str(dd)+'%2F'+str(mm)+'%2F'+str(yyyy)+'&ADT=1&CHD=0&INF=0&class=Economy&source=fresco-home&version=1.8'
@@ -14,9 +15,17 @@ def get_source(departure,destination,dd,mm,yyyy):
     source_code = driver.page_source
     time.sleep(1)
     driver.close()
+    try:
+        for i in source_code:
+            print(i, end="")
+    # print(source_code)
+    except:
+        print()
     return(source_code)
 def main():
     source = get_source('BOM','DEL',28,11,2019)
+    # soup = BeautifulSoup(source, 'html5lib')
+    # print(soup)
 
 if __name__ == "__main__":
     main()
