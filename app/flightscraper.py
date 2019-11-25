47edd3747b78d94672ed141d5eae5d5ede2f9c42
Yashvardhan Jain <yashvardhan18@yahoo.com>
2019-11-25 09:01:41 +0530

    Updated FLight Scraper

--- app/flightscraper.py
+++ app/flightscraper.py
@@ -1,48 +1,53 @@
 from bs4 import BeautifulSoup
 
-soup = BeautifulSoup(aa, 'html5lib')
-
-flights = []
-
-for row1 in soup.findAll('div', attrs = {'class':'schedule v-aligm-t pr'}):
-
-    flight = []
-
-    t1 = row1.findAll('div', attrs = {'class':'i-b pr'})
-    for row in t1:
-        dt = str(row)
-        dt = dt[20:25]
-
-        ar = str(row.p)
-        ar = ar[71:]
-        ar = ar[:ar.find('<')]
-
-        fn = row.span.text
 
 
-    t2 = row1.findAll('p', attrs = {'class':'fs-10 font-lightgrey no-wrap city ellipsis'})
-    for row in t2:
-        flight.append(row.text)
+def flightData(rawData):
+    soup = BeautifulSoup(rawData, 'html5lib')
 
-    t3 = row1.findAll('p', attrs = {'autom':'arrivalTimeLabel'})
-    for row in t3:
-        at = row.text
+    flights = [] # Departure City, Arrival City, Airline, Flight No, Departure Time, Arrival Time, Flight Duration,, No of stops
 
-    t4 = row1.findAll('p', attrs = {'autom':'durationLabel'})
-    for row in t4:
-        dr = row.text
+    for row1 in soup.findAll('div', attrs = {'class':'schedule v-aligm-t pr'}):
 
-    t5 = row1.findAll('span', attrs = {'class':'cursor-default'})
-    for row in t5:
-        st = row.text
+        flight = []
 
-    flight.append(ar)
-    flight.append(fn)
-    flight.append(dt)
-    flight.append(at)
-    flight.append(dr)
-    flight.append(st)
+        t1 = row1.findAll('div', attrs = {'class':'i-b pr'})
+        for row in t1:
+            dt = str(row)
+            dt = dt[20:25]
 
-    flights.append(flight)
+            ar = str(row.p)
+            ar = ar[71:]
+            ar = ar[:ar.find('<')]
 
-print(flights)
+            fn = row.span.text
+
+
+        t2 = row1.findAll('p', attrs = {'class':'fs-10 font-lightgrey no-wrap city ellipsis'})
+        for row in t2:
+            flight.append(row.text)
+
+        t3 = row1.findAll('p', attrs = {'autom':'arrivalTimeLabel'})
+        for row in t3:
+            at = row.text
+
+        t4 = row1.findAll('p', attrs = {'autom':'durationLabel'})
+        for row in t4:
+            dr = row.text
+
+        t5 = row1.findAll('span', attrs = {'class':'cursor-default'})
+        for row in t5:
+            st = row.text
+
+        flight.append(ar)
+        flight.append(fn)
+        flight.append(dt)
+        flight.append(at)
+        flight.append(dr)
+        flight.append(st)
+
+        flights.append(flight)
+
+    # print(flights)
+
+    return(flights)
