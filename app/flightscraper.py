de1b23d3a563904bc6f2cda7873130cb3ba5679b
Yashvardhan Jain <yashvardhan18@yahoo.com>
2019-11-25 09:17:12 +0530

    Updated Flight Scraper function

--- app/flightscraper.py
+++ app/flightscraper.py
@@ -1,5 +1,15 @@
 from bs4 import BeautifulSoup
+from selenium import webdriver as wd
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait as wait
+from selenium.webdriver.support.expected_conditions import presence_of_element_located
+import time
+from bs4 import BeautifulSoup
 
+
+global driver
+driver = ""
 
 
 def flightData(rawData):
@@ -51,3 +61,40 @@ def flightData(rawData):
     # print(flights)
 
     return(flights)
+
+
+def get_source(departureCode, arrivalCode, dd, mm, yyyy):
+
+    driver = wd.Firefox()
+
+    url = 'https://flight.yatra.com/air-search-ui/dom2/trigger?type=O&viewName=normal&flexi=0&noOfSegments=1&origin=' + str(departureCode)+ '&originCountry=IN&destination=' + str(arrivalCode) + '&destinationCountry=IN&flight_depart_date='+str(dd)+'%2F'+str(mm)+'%2F'+str(yyyy)+'&ADT=1&CHD=0&INF=0&class=Economy&source=fresco-home&version=1.8'
+    driver.get(url)
+    source_code = driver.page_source
+    time.sleep(1)
+    driver.close()
+    modSource = ""
+    try:
+        for i in source_code:
+            modSource = modSource + i
+    except:
+        bool = 0
+    return(modSource)
+
+
+def flightSearch(departureCode, arrivalCode, dd, mm, yyyy):
+
+    source = get_source(departureCode, arrivalCode, dd, mm, yyyy)
+    flights = flightData(source)
+
+    return(flights)
+
+def testCase():
+    departureCode = "DEL"
+    arrivalCode = "BOM"
+    dd = 26
+    mm = 11
+    yyyy = 2019
+    flights = flightSearch(departureCode, arrivalCode, dd, mm, yyyy)
+    print(flights)
+
+testCase()
