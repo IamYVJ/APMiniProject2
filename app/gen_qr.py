70940eba3053636cfd7b87cf5a65bbcde45ca4dc
Sai Khurana <sai.khurana_ug21@ashoka.edu.in>
2019-11-30 03:18:00 +0530

    Added QR Code Generator

---
+++ app/gen_qr.py
@@ -0,0 +1,12 @@
+import pyqrcode
+
+def genQR(user_id, departure,destination,flight_duration,departure_time,arrival_time,date,output,scale):
+    final_str = str(user_id) + ':' + str(departure) + ':' + str(destination) + ':' + str(flight_duration) + 'hrs' + ':' + 'DEP ' + str(departure_time) + ':' + 'ARR ' +str(arrival_time) + ':' + str(date)
+    big_code = pyqrcode.create(final_str, mode='binary')
+    big_code.png('app/static/QR/' + output + '.png', scale=scale)
+    return
+
+
+if __name__ == "__main__":
+    genQR('1' , 'Bombay' , 'New Delhi' , '48' , '14:40' , '17:00' ,'01-12-2019' ,'BOM-DEL', 5)
+
