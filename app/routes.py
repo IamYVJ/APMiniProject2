4cd4c563db2ff43eb3cf351ad2f8305bcc925da0
Yash Burad <yash.burad_ug21@ashoka.edu.in>
2019-11-30 12:54:48 +0530

    Added trainsearch and booking html

--- app/routes.py
+++ app/routes.py
@@ -12,6 +12,7 @@ from app.forms import ResetPasswordRequestForm
 from app.emailPasswordReset import send_password_reset_email
 from app.forms import ResetPasswordForm
 from app.flightscraper import flightSearch
+from app.trainscraper import trainSearch
 
 app.config['OAUTH_CREDENTIALS'] = {
     'google': {
@@ -19,6 +20,7 @@ app.config['OAUTH_CREDENTIALS'] = {
         'secret': 'gJ5hLx6ikRTAC8NlONLZ67Kx'
     }}
 
+a=" dddd"
 
 @app.route('/')
 @app.route('/index')
@@ -201,6 +203,75 @@ def reset_password(token):
     return render_template('reset_password.html', title = 'Reset Password', form=form)
 
 @app.route('/payment',methods = ['POST', 'GET'])
+@login_required
 def payment():
-    print("111")
-    return render_template('payment.html')
+    flight =  str(request.args.get('flightid'))
+    print(flight)
+    with sqlite3.connect('app/site.db') as conn:
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM flights WHERE flightid =" +str(flight))
+        row=cur.fetchone()    
+    print(row)
+    return render_template('payment.html', row=row)
+
+@app.route('/train',methods = ['POST', 'GET'])
+def train():
+    # if request.method == 'POST':
+    #   result = request.form
+    # print(flights)
+    # with sqlite3.connect('app/site.db') as conn:
+    #     cur = conn.cursor()
+    #     cur.execute("DELETE from flights")
+    dtoday = str(date.today())
+    print(dtoday)
+    # if form.validate_on_submit():
+    #     return form.dt.data.strftime('%Y-%m-%d')
+    return render_template('train.html', dtoday = dtoday)
+
+@app.route('/trainsearch',methods = ['POST', 'GET'])
+def trainsearch():
+    srcStn="R"
+    srcCity=""
+    destStn="HWH"
+    destCity=""
+    dep=""
+    ret=""
+    dtoday = str(date.today())
+    if request.method == 'POST':
+        srcCity = request.form['from']
+        destCity = request.form['to']
+        dep=request.form['tday'].split('-')
+    print(srcCity)
+    print(destCity)
+    print(dep)
+    
+    # with sqlite3.connect('app/site.db') as conn:
+    #     cur = conn.cursor()
+    #     cur.execute("SELECT count(*) FROM flights")
+    #     row=cur.fetchone()
+    #     print(row[0])
+    # if(row[0]==0):
+    #     if(departureCode=="" or arrivalCode=="" or len(dep)==0):
+    #         return render_template('flights.html',form=form, dtoday = dtoday)
+    #     # else:
+    #         # flights = flightSearch(departureCode, arrivalCode, dep[2], dep[1], dep[0])
+    # else:
+    #     with sqlite3.connect('app/site.db') as conn:
+    #         cur = conn.cursor()
+    #         cur.execute("SELECT * FROM flights")
+    #         row=cur.fetchall()
+    #         for rows in row:
+    #             flights.append(rows)
+    # print(flights)
+    # flights=[[0, 'New Delhi', 'Mumbai', 'Go Air', 'G8-429', '21:35', '23:50', '2h 15m ', 'Non Stop ', '5308'], [1, 'New Delhi', 'Mumbai', 'Go Air', 'G8-338', '10:30', '12:50', '2h 20m ', 'Non Stop ', '5308'], [2, 'New Delhi', 'Mumbai', 'Go Air', 'G8-640', '18:00', '20:20', '2h 20m ', 'Non Stop ', '5308'], [3, 'New Delhi', 'Mumbai', 'Go Air', 'G8-544', '22:40', '01:00+ 1 day', '2h 20m ', 'Non Stop ', '5308'], [4, 'New Delhi', 'Mumbai', 'Go Air', 'G8-446', '19:40', '22:05', '2h 25m ', 'Non Stop ', '5308'], [5, 'New Delhi', 'Mumbai', 'Vistara', 'UK-975', '06:00', '08:00', '2h 00m ', 'Non Stop ', '5518'], [6, 'New Delhi', 'Mumbai', 'Air India', 'AI-887', '07:00', '09:05', '2h 05m ', 'Non Stop ', '5518'], [7, 'New Delhi', 'Mumbai', 'Vistara', 'UK-985', '19:50', '21:55', '2h 05m ', 'Non Stop ', '5518'], [8, 'New Delhi', 'Mumbai', 'Air India', 'AI-805', '20:00', '22:10', '2h 10m ', 'Non Stop ', '5518'], [9, 'New Delhi', 'Mumbai', 'Air India', 'AI-191', '21:00', '23:10', '2h 10m ', 'Non Stop ', '5518'], [10, 'New Delhi', 'Mumbai', 'Vistara', 'UK-927', '09:30', '11:40', '2h 10m ', 'Non Stop ', '5518'], [11, 'New Delhi', 'Mumbai', 'Vistara', 'UK-953', '20:40', '22:50', '2h 10m ', 'Non Stop ', '5518'], [12, 'New Delhi', 'Mumbai', 'Vistara', 'UK-981', '21:40', '23:50', '2h 10m ', 'Non Stop ', '5518'], [13, 'New Delhi', 'Mumbai', 'Vistara', 'UK-933', '15:30', '17:40', '2h 10m ', 'Non Stop ', '5518'], [14, 'New Delhi', 'Mumbai', 'Go Air', 'G8-530', '07:00', '09:10', '2h 10m ', 'Non Stop ', '5518'], [15, 'New Delhi', 'Mumbai', 'Air India', 'AI-665', '08:00', '10:15', '2h 15m ', 'Non Stop ', '5518'], [16, 'New Delhi', 'Mumbai', 'Air India', 'AI-24', '18:00', '20:15', '2h 15m ', 'Non Stop ', '5518'], [17, 'New Delhi', 'Mumbai', 'Vistara', 'UK-995', '10:20', '12:35', '2h 15m ', 'Non Stop ', '5518'], [18, 'New Delhi', 'Mumbai', 'Vistara', 'UK-993', '12:45', '15:00', '2h 15m ', 'Non Stop ', '5518'], [19, 'New Delhi', 'Mumbai', 'Vistara', 'UK-923', '06:40', '09:00', '2h 20m ', 'Non Stop ', '5518'], [20, 'New Delhi', 'Mumbai', 'Vistara', 'UK-963', '08:50', '11:10', '2h 20m ', 'Non Stop ', '5518'], [21, 'New Delhi', 'Mumbai', 'Vistara', 'UK-945', '11:40', '14:00', '2h 20m ', 'Non Stop ', '5518'], [22, 'New Delhi', 'Mumbai', 'Vistara', 'UK-955', '17:45', '20:05', '2h 20m ', 'Non Stop ', '5518'], [23, 'New Delhi', 'Mumbai', 'Vistara', 'UK-977', '18:55', '21:15', '2h 20m ', 'Non Stop ', '5518'], [24, 'New Delhi', 'Mumbai', 'Air Asia', 'I5-881', '05:20', '07:30', '2h 10m ', 'Non Stop ', '5600']]
+    # with sqlite3.connect('app/site.db') as conn:
+    #     cur = conn.cursor()
+    #     for rows in flights:
+    #         cur.execute("INSERT INTO flights (flightid, destination ,arrival,airlines,flightno,depart,arrive,duration ,type,price) VALUES (?, ?, ?, ?, ?, ?,?,?,?,?)", (rows[0],rows[1],rows[2],rows[3],rows[4],rows[5],rows[6],rows[7],rows[8],rows[9]))
+    # print(flights)
+    # train=trainSearch(srcStn, srcCity, destStn, destCity, dep[2], dep[1], dep[0])
+    # global a
+    print(a)
+    train=[['GITANJALI EX', '#12859', 'S   M   T   W   T   F   S', '11:35 PM  Sun', 'RYou searched for trains departing from R(Raipur), but this train departs from R (Raipur Jn)R R HWHRaipur Jn', '12h 55m', '12:30 PM  Mon', 'HWHYou searched for trains arriving in HWH (Kolkata), but this train arrives in HWH (Howrah Jn).HWH R HWHHowrah Jn', [['Sleeper', '440', 'RLWL 95', 'Updated 3 hrs ago'], ['3 Tier AC', '1165', 'RLWL 24', 'Updated 15 hrs ago'], ['2 Tier AC', '1660', 'RLWL 16', 'Updated 18 hrs ago']]], ['JNANESWARI DEL', '#12101', 'S   T   W   S', '2:30 PM  Sun', 'RYou searched for trains departing from R(Raipur), but this train departs from R (Raipur Jn)R R HWHRaipur Jn', '13h 5m', '3:35 AM  Mon', 'HWHYou searched for trains arriving in HWH (Kolkata), but this train arrives in HWH (Howrah Jn).HWH R HWHHowrah Jn', [['Sleeper', '440', 'RLWL 41', 'Updated 11 hrs ago'], ['3 Tier AC', '1165', 'RLWL 22', 'Updated 1 day ago'], ['2 Tier AC', '1660', 'RLWL 6', 'Updated 17 hrs ago'], ['1st Class AC', '2815', 'RLWL 3', 'Updated 1 day ago']]], ['SNSI HWH SUP EX', '#22893', 'S', '6:10 AM  Sun', 'RYou searched for trains departing from R(Raipur), but this train departs from R (Raipur Jn)R R HWHRaipur Jn', '13h 20m', '7:30 PM  Sun', 'HWHYou searched for trains arriving in HWH (Kolkata), but this train arrives in HWH (Howrah Jn).HWH R HWHHowrah Jn', [['Sleeper', '445', 'RLWL 90', 'Updated 11 hrs ago'], ['3 Tier AC', '1175', 'RLWL 36', 'Updated 11 hrs ago'], ['2 Tier AC', '1675', 'Booking not allowed', 'Updated 17 hrs ago']]], ['AZAD HIND EX', '#12129', 'S   M   T   W   T   F   S', '2:52 PM  Sun', 'RYou searched for trains departing from R(Raipur), but this train departs from R (Raipur Jn)R R HWHRaipur Jn', '13h 23m', '4:15 AM  Mon', 'HWHYou searched for trains arriving in HWH (Kolkata), but this train arrives in HWH (Howrah Jn).HWH R HWHHowrah Jn', [['Sleeper', '440', 'RLWL 44', 'Updated 4 hrs ago'], ['3 Tier AC', '1165', 'Booking not allowed', 'Updated 4 hrs ago'], ['2 Tier AC', '1660', 'RLWL 9', 'Updated 11 hrs ago']]], ['HOWRAH MAI', '#12809', 'S   M   T   W   T   F   S', '4:10 PM  Sun', 'RYou searched for trains departing from R(Raipur), but this train departs from R (Raipur Jn)R R HWHRaipur Jn', '13h 40m', '5:50 AM  Mon', 'HWHYou searched for trains arriving in HWH (Kolkata), but this train arrives in HWH (Howrah Jn).HWH R HWHHowrah Jn', [['Sleeper', '440', 'RLWL 39', 'Updated 11 hrs ago'], ['3 Tier AC', '1165', 'RLWL 24', 'Updated 18 hrs ago'], ['2 Tier AC', '1660', 'RLWL 9', 'Updated 18 hrs ago'], ['1st Class AC', '2815', 'RLWL 2', 'Updated 1 day ago']]], ['HOWRAH EXPRES', '#12833', 'S   M   T   W   T   F   S', '11:05 PM  Sun', 'RYou searched for trains departing from R(Raipur), but this train departs from R (Raipur Jn)R R HWHRaipur Jn', '14h 25m', '1:30 PM  Mon', 'HWHYou searched for trains arriving in HWH (Kolkata), but this train arrives in HWH (Howrah Jn).HWH R HWHHowrah Jn', [['Sleeper', '445', 'RLWL 69', 'Updated 5 hrs ago'], ['3 Tier AC', '1175', 'RLWL 15', 'Updated 13 hrs ago'], ['2 Tier AC', '1675', 'RLWL 16', 'Updated 13 hrs ago']]], ['LTT SHALIMAR E', '#18029', 'S   M   T   W   T   F   S', '7:42 PM  Sun', 'RYou searched for trains departing from R(Raipur), but this train departs from R (Raipur Jn)R R HWHRaipur Jn', '16h 38m', '12:20 PM  Mon', 'SHMDifferentYou searched for trains arriving in HWH (Kolkata), but this train arrives in SHM (Shalimar).HWH R SHMShalimar', [['Sleeper', '410', 'RLWL 55', 'Updated 9 hrs ago'], ['3 Tier AC', '1120', 'RLWL 17', 'Updated 9 hrs ago'], ['2 Tier AC', '1615', 'RLWL 8', 'Updated 9 hrs ago']]]]
+    a=train
+    return render_template('trainsearch.html', flights=train)
