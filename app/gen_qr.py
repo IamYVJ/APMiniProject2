61c31fafe75cebd321c9fd275ec269107da4be92
Yashvardhan Jain <yashvardhan18@yahoo.com>
2019-11-30 18:51:49 +0530

    QR Code Generator Updated

--- app/gen_qr.py
+++ app/gen_qr.py
@@ -1,7 +1,7 @@
 import pyqrcode
 
-def genQR(user_id,pnr,firstname,lastname, departure,destination,flight_duration,departure_time,arrival_time,date,scale):
-    final_str = str(user_id) + ':' + str(pnr) + ':'   + str(firstname) + ':'  + str(lastname) + ':'  + str(departure) + ':' + str(destination) + ':' + str(flight_duration) + 'hrs' + ':' + 'DEP ' + str(departure_time) + ':' + 'ARR ' +str(arrival_time) + ':' + str(date)
+def genQR(user_id, pnr,firstname, lastname, departure, arrival, flight_duration, departure_time, arrival_time, date, scale):
+    final_str = 'PNR: ' + str(pnr) + '     Date: ' + str(date) + '\nName: '   + str(firstname) + ' '  + str(lastname) + '\nFrom: '  + str(departure) + '     To: ' + str(arrival) + '\nDeparture: ' + str(departure_time) + '     Arrival: ' + str(arrival_time) + '\nDuration: ' + str(flight_duration) 
     big_code = pyqrcode.create(final_str, mode='binary')
     big_code.png('app/static/QR/' + pnr + '.png', scale=scale)
     return
@@ -9,4 +9,3 @@ def genQR(user_id,pnr,firstname,lastname, departure,destination,flight_duration,
 
 # if __name__ == "__main__":
 #     genQR('1' , 'Bombay' , 'New Delhi' , '48' , '14:40' , '17:00' ,'01-12-2019' ,'BOM-DEL', 5)
-
